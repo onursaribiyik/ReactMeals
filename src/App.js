60c708d4345deccdfcb87f41f387dfda33f1752c
Onur Sarıbıyık <110836040+onursaribiyik@users.noreply.github.com>
2023-06-03 16:27:09 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+
+function App() {
+
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const shownCartHandler = () => {
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={shownCartHandler}/>
+      <main>
+        <Meals/>
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
